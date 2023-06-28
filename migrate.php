fcd66a170bc7d8c9fc1ee1cef8a2e28db1b5906f
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-28 14:14:53 +0200

    fix

--- migrate.php
+++ migrate.php
@@ -35,17 +35,43 @@ $statements = [
         FOREIGN KEY (fk_image_owner) REFERENCES users(username)
     )',
 ];
-#####
+
+function parse(string $url): array
+{
+    $params = array();
+    $regex = "/(.+):\/\/(.+):(.+)@(.+):(\d+)\/(.*)/";
+    preg_match($regex, $url, $matches);
+
+    if ($matches[1] == "postgres") {
+        $params["scheme"] = "pgsql";
+    }
+
+    $params["user"] = $matches[2];
+    $params["password"] = $matches[3];
+    $params["host"] = $matches[4];
+    $params["port"] = $matches[5];
+    $params["database"] = $matches[6];
+
+    return $params;
+}
+
+function from_params(array $params): string
+{
+    return $params["scheme"] . ":host=" . $params["host"] . ";port=" . $params["port"] . ";dbname=" . $params["database"];
+}
 
 try {
-    $DB_NAME = getenv('DB_NAME');
-    $DB_USER = getenv('DB_USER');
-    $DB_PASS = getenv('DB_PASS');
-    $DB_HOST = getenv('DB_HOST');
-    $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
+    // Extract DATABASE_URL env variable
+    $url = getenv("DATABASE_URL");
+
+    // Parse url
+    $params = parse($url);
+
+    // Create DSN from params
+    $DSN = from_params($params);
 
     // Make a database connection
-    $db = new PDO($DSN, $DB_USER, $DB_PASS);
+    $db = new PDO($DSN, $params["user"], $params["password"]);
 
     // Set PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
