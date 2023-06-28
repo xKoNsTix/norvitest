b12d9034a57970b2bec2e181c5f6a1575e790262
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-28 14:04:43 +0200

    .

--- src/config.php
+++ src/config.php
@@ -1,14 +1,41 @@
 <?php
-$DB_NAME = getenv('DB_NAME');
-$DB_USER = getenv('DB_USER');
-$DB_PASS = getenv('DB_PASS');
-$DB_HOST = getenv('DB_HOST');
 
-$DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
+
+$url = getenv("DATABASE_URL");
+
+
+// make a database connection
+
+function parse(string $url): array
+{
+    $params = array();
+    $regex = "/(.+):\/\/(.+):(.+)@(.+):(\d+)\/(.*)/";
+    preg_match($regex, $url, $matches);
+
+    if ($matches[1] == "postgres") {
+        $params["scheme"] = "pgsql";
+    }
+
+    $params["user"] = $matches[2];
+    $params["password"] = $matches[3];
+    $params["host"] = $matches[4];
+    $params["port"] = $matches[5];
+    $params["database"] = $matches[6];
+
+    return $params;
+}
+
+function from_params(array $params): string
+{
+    return $params["scheme"] . ":host=" . $params["host"] . ";port=" . $params["port"] . ";dbname=" . $params["database"];
+}
+
+$params = parse($url);
+$dsn = from_params($params);
 try {
-    $db = new PDO($DSN, $DB_USER, $DB_PASS);
+    $pdo = new PDO($dsn, $params['user'], $params['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
     $currentDir = getcwd();
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
-?>
